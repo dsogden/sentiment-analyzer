92bcf9a7fba134171f511986572faa75045dd967
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-11-20 17:21:31 +0000

    testing to make sure torch works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,25 @@
 import streamlit as st
-import torch
+import transformers
 
 
-st.title("Torch Example")
+st.title("Sentiment Analysis Application")
 # st.write(
 #     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 # )
 
-x = torch.randn(size=(10, 10))
-st.write(x)
+tokenizer = transformers.DistilBertTokenizer.from_pretrained(
+    "distilbert-base-uncased-finetuned-sst-2-english"
+)
+
+model = transformers.DistilBertForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased-finetuned-sst-2-english"
+)
+
+st.write("Enter a text below to analyze its sentiment:")
+user_input = st.text_area("Text to analyze", placeholder="Type something...")
+if st.button('Analyze Sentiment'):
+    encoded = tokenizer(user_input, return_tensors="pt")
+    output = model(**encoded)
+    predicted_class_id = output['logits'].argmax().item()
+    prediction = model.config.id2label[predicted_class_id]
+    st.write(prediction)
