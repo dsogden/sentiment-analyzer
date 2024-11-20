68226c95c8dd9c87a5eecbf29c1b0f57c1a4debb
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-11-20 17:13:21 +0000

    testing to make sure torch works

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ st.title("Torch Example")
 #     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 # )
 
-x = torch.arange(10)
+x = torch.randn(size=(10, 10))
 st.write(x)
