4006af6e9b6f326b757b726b65c724841fcf0498
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-11-20 17:12:43 +0000

    testing to make sure torch works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import torch
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Torch Example")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+x = torch.arange(10)
+st.write(x)
